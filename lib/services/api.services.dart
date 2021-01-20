106837384ba5a9184c1d9a01b243f0f02ecfb066
ibrahim.a <ibrahim.a@bestcomp.net>
2021-01-20 19:45:01 +0400

    post request added

--- lib/services/api.services.dart
+++ lib/services/api.services.dart
@@ -1,9 +1,73 @@
+// import 'dart:convert';
+
+// import 'package:my_note/models/note_modify.dart';
+// import 'package:my_note/models/notes.dart';
+// // import 'package:my_note/services/glob_value.dart';
+// import 'package:http/http.dart' as http;
+
+// class ApiService {
+//   static ApiService _instance;
+
+//   ApiService._();
+
+//   static ApiService get getInstance => _instance = _instance ?? ApiService._();
+//   final baseUrl = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app';
+//   Map<String, String> headers() {
+//     // final apiKey = LocalData.getInstance.apiKey;
+//     return {
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json',
+//       'apiKey': 'bee3a2cd-9e8d-4c00-9811-3f2c4dc2d437',
+//     };
+//   }
+
+//   Future<List<Notes>> getNotes() async {
+//     final response = await http.get(
+//       Uri.encodeFull(baseUrl + '/notes'),
+//       headers: headers(),
+//     );
+//     if (response.statusCode == 200) {
+//       final _body = response.body;
+//       final _noteResponse = notesFromJson(_body);
+//       return _noteResponse;
+//     }
+//     throw Exception('Error Occured: ${response.statusCode}');
+//   }
+
+//   Future<AddNote> addNote(AddNote note) async {
+//     final response = await http.post(Uri.encodeFull(baseUrl + '/notes'),
+//         headers: headers(),
+//         body: note.toJson(),
+//         );
+//     if (response.statusCode == 200) {
+//       print('Request done');
+//     }
+//     throw Exception('Error :${response.statusCode}');
+//   }
+
+// /*
+
+//   Future<APIResponse<bool>> createNote(NoteManipulation item) {
+//     return http.post(API + '/notes', headers: headers, body: json.encode(item.toJson())).then((data) {
+//       if (data.statusCode == 201) {
+//         return APIResponse<bool>(data: true);
+//       }
+//       return APIResponse<bool>(error: true, errorMessage: 'An error occured');
+//     })
+//     .catchError((_) => APIResponse<bool>(error: true, errorMessage: 'An error occured'));
+//   }
+
+// */
+
+// }
+
 import 'dart:convert';
 
-import 'package:my_note/models/note_modify.dart';
-import 'package:my_note/models/notes.dart';
-// import 'package:my_note/services/glob_value.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+import '../models/note_modify.dart';
+import '../models/notes.dart';
 
 class ApiService {
   static ApiService _instance;
@@ -35,15 +99,20 @@ class ApiService {
   }
 
   Future<AddNote> addNote(AddNote note) async {
-    final response = await http.post(Uri.encodeFull(baseUrl + '/notes'),
-        headers: headers(),
-        body: json.encode(
-          {"noteTitle": note.noteTitle, "noteContent": note.noteContent},
-        ));
-    if (response.statusCode == 200) {
-      print('Request done');
+    final response = await http.post(
+      Uri.encodeFull(baseUrl + '/notes'),
+      headers: headers(),
+      body: jsonEncode(
+        {"noteTitle": note.noteTitle, "noteContent": note.noteContent},
+      ),
+    );
+    if (response.statusCode == 201) {
+      final _body = response.body;
+      final _addNoteResponse = addNoteFromJson(_body);
+      debugPrint("Not başarıyla eklendi");
+      return _addNoteResponse;
     }
-    throw Exception('Error :${response.statusCode}');
+    throw Exception('Error Occured: ${response.statusCode}');
   }
 
 /*
--- lib/view/note_add.dart
+++ lib/view/note_add.dart
@@ -1,71 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:my_note/models/note_modify.dart';
-
-import 'package:my_note/state/app.state.dart';
 import 'package:provider/provider.dart';
 
+import '../models/note_modify.dart';
+import '../state/app.state.dart';
+import 'home.dart';
+
 // ignore: must_be_immutable
-class NoteModify extends StatefulWidget {
+
+class NoteModify extends StatelessWidget {
   final String noteID;
   NoteModify({this.noteID, this.state});
   final AppState state;
 
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _contentController = TextEditingController();
-
-  bool _isLoading = false;
-  @override
-  _NoteModifyState createState() => _NoteModifyState();
-}
-
-class _NoteModifyState extends State<NoteModify> {
-  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(4, 9, 21, 1),
       appBar: AppBar(title: Text('Create note')),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: widget._isLoading
-            ? Center(child: CircularProgressIndicator())
-            : Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    color: Colors.white,
-                    child: TextField(
-                      controller: widget._titleController,
-                      decoration: InputDecoration(hintText: 'Note title'),
+      body: Consumer<AppState>(
+        builder: (context, model, child) => Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: model.isloading
+              ? Center(child: CircularProgressIndicator())
+              : Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      color: Colors.white,
+                      child: TextField(
+                        controller: _titleController,
+                        decoration: InputDecoration(hintText: 'Note title'),
+                      ),
                     ),
-                  ),
-                  Container(height: 8),
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    color: Colors.white,
-                    child: TextField(
-                      controller: widget._contentController,
-                      decoration: InputDecoration(
-                          hintText: 'Note content', fillColor: Colors.white),
+                    Container(height: 8),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      color: Colors.white,
+                      child: TextField(
+                        controller: _contentController,
+                        decoration: InputDecoration(
+                          hintText: 'Note content',
+                          fillColor: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                  Container(height: 16),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 35,
-                    child: RaisedButton(
-                        child: Text('Submit',
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                            )),
+                    Container(height: 16),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 35,
+                      child: RaisedButton(
+                        child: Text(
+                          'Submit',
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
                         color: Colors.white,
-                        onPressed: () async {
-                          widget.state.adNote(
+                        onPressed: () {
+                          model.adNote(
                             AddNote(
-                              noteTitle: widget._titleController.text,
-                              noteContent: widget._contentController.text,
+                              noteTitle: _titleController.text,
+                              noteContent: _contentController.text,
                             ),
                           );
+                          model.fetchNotes();
                           // final note = AddNote(
                           //     noteTitle: widget._titleController.text,
                           //     noteContent: widget._contentController.text);
@@ -73,11 +73,17 @@ class _NoteModifyState extends State<NoteModify> {
                           // Provider.of<AppState>(context, listen: false)
                           //     .addNote();
 
-                          Navigator.of(context).pop();
-                        }),
-                  )
-                ],
-              ),
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
+        ),
       ),
     );
   }
